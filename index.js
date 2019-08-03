a3dec1c5d60beaeb0eefe0308cca00a26526f08e
github username <github email address>
2019-08-02 22:31:55 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+//! for ([initialization]; [condition]; [iteration]) {
+//!     [loop body]
+//!   }
+// returns an array of thank you messages for each name provided to the function:
+function writeCards(namesArray, adjective) {
+    let cards = []
+    // assign empty array to cards so we can add messages later
+    for(let i = 0; i < namesArray.length; i++) {
+    let message = `Thank you, ${namesArray[i]}, for the wonderful ${adjective} gift!`;
+    cards.push(message)
+    }
+    return cards;
+}
+
+//  invokes console.log once for each number, 
+// counting down from the number provided to zero:
+function countdown(number) {
+    for(let i = 0; i <= number; i++) {
+        console.log(number - i)
+    }
+}
