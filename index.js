8d7dfbab21580d174bd88c538c9ba04b65bfcafe
Malisa Middlebrooks <middlebrooks314@gmail.com>
2019-08-04 02:59:47 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+
+function writeCards(array, event) {
+  let thankYous = [];
+  for (let i = 0; i < array.length; i++) {
+    thankYous.push(`Thanks ${array}! for ${event} gift!`);
+  }
+  return thankYous;
+}
+
+function countdown(integer) {
+  while (integer > 0) {
+    console.log(integer);
+    integer -= 1;
+  }
+  console.log(integer);
+  
+}
