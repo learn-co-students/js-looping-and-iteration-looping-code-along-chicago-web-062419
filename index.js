d9efec72a123c93fc5b7d6fa03fe5d467c243f04
Iuliu Padurean <ciprian.padurean@gmail.com>
2019-08-05 00:50:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countdown(num) {
+  let i = 0
+  while (i < num+1){
+    console.log(i++)
+  }
+}
+
+function writeCards (array, string) {
+    var newArr = []
+    for (let i=0; i<array.length; i++)  {
+    newArr.push (`Thank you, ${array[i]}, for the wonderful ${string} gift!`);
+
+    }
+    return newArr;
+}
