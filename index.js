123e40f3a206dcbb91026a7ecadb7fc4804379e5
Philip Roth <50246817+rothberry@users.noreply.github.com>
2019-08-06 09:37:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+// const names = ["Ada", "Brendan", "Ali"]
+
+function writeCards(names, ele) {
+    let arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${ele} gift!`);
+    }
+    return arr
+}
+// console.log(writeCards(names, "birthday"));
+
+function countdown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    }
+}
+// countdown(10)
